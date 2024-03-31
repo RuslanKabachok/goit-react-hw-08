35b6e29eb863dddf61bf5160809162dc048f56e1
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-03-31 07:44:00 +0300

    upd

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -7,7 +7,7 @@ const setAuthHeader = (token) => {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
 
-const clearAuthHeader = (token) => {
+const clearAuthHeader = () => {
   axios.defaults.headers.common['Authorization'] = '';
 };
 
